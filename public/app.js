81ed898882716e1d75c619f259851d229eb6d641
GyeongEun-Kim <kekim20@naver.com>
2023-11-30 17:45:18 +0900

    [ADD] : 리스트 가져오는 api 구현

--- public/app.js
+++ public/app.js
@@ -1,12 +1,7 @@
 const selectbox = document.getElementById("search");
 
-selectbox.addEventListener("change", (event) => {
-  const target = event.target.value;
-  console.log(target);
-});
-
-// eslint-disable-next-line no-unused-vars
-const makeOptions = (method, body) => {
+// eslint-disable-next-line no-unused-vars, import/prefer-default-export
+export const makeOptions = (method, body) => {
   return {
     method,
     headers: {
@@ -15,3 +10,29 @@ const makeOptions = (method, body) => {
     body,
   };
 };
+
+const listByPartCode = async (partCode) => {
+  const option = makeOptions("POST", JSON.stringify({ partCode }));
+  const URL = "/list";
+
+  fetch(URL, option)
+    .then((response) => response.json())
+    .then((data) => console.log(data[0].resultList));
+};
+
+// eslint-disable-next-line no-shadow
+// 아직 사용X
+const detailByPartCodeAndContentNum = async (partCode, contentNum) => {
+  const option = makeOptions("POST", JSON.stringify({ partCode, contentNum }));
+  const URL = "/detail";
+
+  fetch(URL, option)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
+selectbox.addEventListener("change", async (event) => {
+  const partCode = event.target.value;
+  await listByPartCode(partCode);
+  // detailByPartCodeAndContentNum(partCode, contentNum);
+});
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-// eslint-disable-next-line import/no-unresolved
-import express from "express";
+const express = require("express");
+// import { makeOptions } from "./public/app.js";
 
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
+
 app.get("/", function (req, res) {
   console.log("http://localhost:3000/ called");
   res.sendFile("index.html");
 });
+
+app.post("/list", (req, res) => {
+  const pageBlock = 5;
+  const page = 1; // 랜덤 값 수정 필요
+  const { partCode } = req.body;
+
+  const request = require("request");
+
+  const URL = `http://pettravel.kr/api/listPart.do?page=${page}&pageBlock=${pageBlock}&partCode=${partCode}`;
+  console.log(URL);
+  // 외부 api서버에 전송
+  request.get(URL, (error, response) => {
+    // console.log(`response :${response}`);
+    res.send(response.body);
+  });
+});
+
 const port = 3000;
 app.listen(port, () =>
   console.log(`http://127.0.0.1:${port}/ app listening on port ${3000}`),
