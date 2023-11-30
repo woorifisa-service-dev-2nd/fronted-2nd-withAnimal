c40773c3ff708793a3774373f4b307ea79e766e6
GyeongEun-Kim <kekim20@naver.com>
2023-11-30 14:28:14 +0900

    [ADD] : html, css 적용

--- public/app.js
+++ public/app.js
@@ -1,3 +1,17 @@
-if (true) {
-  console.log("ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ");
-}
+const selectbox = document.getElementById("search");
+
+selectbox.addEventListener("change", (event) => {
+  const target = event.target.value;
+  console.log(target);
+});
+
+// eslint-disable-next-line no-unused-vars
+const makeOptions = (method, body) => {
+  return {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  };
+};
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-unresolved
 import express from "express";
 
 const app = express();
