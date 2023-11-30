6ae1b54306a7dde39ce7ec5b8f9ff9d7cd6680e4
ehyongyong <yyoonngg3553@naver.com>
2023-11-30 14:39:34 +0900

    html/css

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import express from "express";
+// eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
+import express from 'express';
+// eslint-disable-next-line linebreak-style
 
 const app = express();
-app.use(express.static("public"));
-app.use(express.json());
-app.get("/", function (req, res) {
-  console.log("http://localhost:3000/ called");
-  res.sendFile("index.html");
+app.use(express.static('public'));
+app.use(express.json())
+app.get('/', (_req, res) => {
+  console.log('http://localhost:3000/ called');
+  res.sendFile('index.html');
 });
 const port = 3000;
-app.listen(port, () =>
-  console.log(`http://127.0.0.1:${port}/ app listening on port ${3000}`),
-);
+app.listen(port, () => console.log(`http://127.0.0.1:${port}/ app listening on port ${3000}`));
