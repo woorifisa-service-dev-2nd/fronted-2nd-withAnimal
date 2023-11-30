447a20a23be74c20000843af3f50d0f70946e15b
a <66014764+anseonyeong@users.noreply.github.com>
2023-11-30 11:01:25 +0900

    seonyeong
    내 코드

---
+++ server.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import express from "express";
+
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.get("/", function (req, res) {
+  console.log("http://localhost:3000/ called");
+  res.sendFile("index.html");
+});
+const port = 3000;
+app.listen(port, () =>
+  console.log(`http://127.0.0.1:${port}/ app listening on port ${3000}`),
+);
